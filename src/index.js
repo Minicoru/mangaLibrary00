44a991f526e664823d3324ef9636c719d99d46c0
Mia Ruiz <miarg49@gmail.com>
2021-10-10 23:48:30 -0300

    sincro notebook

--- src/index.js
+++ src/index.js
@@ -15,32 +15,38 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import AdminLayout from "layouts/Admin/Admin.js";
-import RTLLayout from "layouts/RTL/RTL.js";
+import ExploreLayout from 'layouts/User/Explore.js';
+import SourcesLayout from 'layouts/User/Sources.js';
+import MyLibraryLayout from 'layouts/User/MyLibrary.js';
+// import AdminLayout from 'layouts/Admin/Admin.js';
+// import RTLLayout from 'layouts/RTL/RTL.js';
 
-import "assets/scss/black-dashboard-react.scss";
-import "assets/demo/demo.css";
-import "assets/css/nucleo-icons.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import 'assets/scss/black-dashboard-react.scss';
+import 'assets/demo/demo.css';
+import 'assets/css/nucleo-icons.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
-import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+import ThemeContextWrapper from './components/ThemeWrapper/ThemeWrapper';
+import BackgroundColorWrapper from './components/BackgroundColorWrapper/BackgroundColorWrapper';
 
 ReactDOM.render(
-  <ThemeContextWrapper>
-    <BackgroundColorWrapper>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-          <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
-          <Redirect from="/" to="/admin/dashboard" />
-        </Switch>
-      </BrowserRouter>
-    </BackgroundColorWrapper>
-  </ThemeContextWrapper>,
-  document.getElementById("root")
+	<ThemeContextWrapper>
+		<BackgroundColorWrapper>
+			<BrowserRouter>
+				<Switch>
+					<Route path='/User/Explore' render={(props) => <ExploreLayout {...props} />} />
+					<Route path='/User/Sources' render={(props) => <SourcesLayout {...props} />} />
+					<Route path='/User/MyLibrary' render={(props) => <MyLibraryLayout {...props} />} />
+					{/* <Route path='/admin' render={(props) => <AdminLayout {...props} />} />
+					<Route path='/rtl' render={(props) => <RTLLayout {...props} />} /> */}
+					<Redirect from='/' to='/User/Explore' />
+				</Switch>
+			</BrowserRouter>
+		</BackgroundColorWrapper>
+	</ThemeContextWrapper>,
+	document.getElementById('root')
 );
--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,9 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+import Explore from "views/Explore.js";
+import MyLibrary from "views/MyLibrary.js";
+import Sources from "views/Sources.js";
 import Dashboard from "views/Dashboard.js";
 import Icons from "views/Icons.js";
 import Map from "views/Map.js";
@@ -26,68 +29,93 @@ import UserProfile from "views/UserProfile.js";
 
 var routes = [
   {
-    path: "/dashboard",
-    name: "Dashboard",
-    rtlName: "لوحة القيادة",
+    path: "/Explore",
+    name: "Explore",
+    rtlName: "Explore",
     icon: "tim-icons icon-chart-pie-36",
-    component: Dashboard,
-    layout: "/admin",
+    component: Explore,
+    layout: "/User/Explore",
   },
   {
-    path: "/icons",
-    name: "Icons",
-    rtlName: "الرموز",
-    icon: "tim-icons icon-atom",
-    component: Icons,
-    layout: "/admin",
+    path: "/MyLibrary",
+    name: "My Library",
+    rtlName: "My Library",
+    icon: "tim-icons icon-chart-pie-36",
+    component: MyLibrary,
+    layout: "/User/MyLibrary",
   },
   {
-    path: "/map",
-    name: "Map",
-    rtlName: "خرائط",
-    icon: "tim-icons icon-pin",
-    component: Map,
-    layout: "/admin",
+    path: "/Sources",
+    name: "Sources",
+    rtlName: "Sources",
+    icon: "tim-icons icon-chart-pie-36",
+    component: Sources,
+    layout: "/User/Sources",
   },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    rtlName: "إخطارات",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: "/admin",
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Table List",
-    rtlName: "قائمة الجدول",
-    icon: "tim-icons icon-puzzle-10",
-    component: TableList,
-    layout: "/admin",
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    rtlName: "طباعة",
-    icon: "tim-icons icon-align-center",
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    path: "/rtl-support",
-    name: "RTL Support",
-    rtlName: "ار تي ال",
-    icon: "tim-icons icon-world",
-    component: Rtl,
-    layout: "/rtl",
-  },
+  
+  // {
+  //   path: "/dashboard",
+  //   name: "Dashboard",
+  //   rtlName: "لوحة القيادة",
+  //   icon: "tim-icons icon-chart-pie-36",
+  //   component: Dashboard,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/icons",
+  //   name: "Icons",
+  //   rtlName: "الرموز",
+  //   icon: "tim-icons icon-atom",
+  //   component: Icons,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/map",
+  //   name: "Map",
+  //   rtlName: "خرائط",
+  //   icon: "tim-icons icon-pin",
+  //   component: Map,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/notifications",
+  //   name: "Notifications",
+  //   rtlName: "إخطارات",
+  //   icon: "tim-icons icon-bell-55",
+  //   component: Notifications,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/user-profile",
+  //   name: "User Profile",
+  //   rtlName: "ملف تعريفي للمستخدم",
+  //   icon: "tim-icons icon-single-02",
+  //   component: UserProfile,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/tables",
+  //   name: "Table List",
+  //   rtlName: "قائمة الجدول",
+  //   icon: "tim-icons icon-puzzle-10",
+  //   component: TableList,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/typography",
+  //   name: "Typography",
+  //   rtlName: "طباعة",
+  //   icon: "tim-icons icon-align-center",
+  //   component: Typography,
+  //   layout: "/admin",
+  // },
+  // {
+  //   path: "/rtl-support",
+  //   name: "RTL Support",
+  //   rtlName: "ار تي ال",
+  //   icon: "tim-icons icon-world",
+  //   component: Rtl,
+  //   layout: "/rtl",
+  // },
 ];
 export default routes;
